8afa12447f9bcb25081ebbc33a5382a7309ecd86
konst-int-i <konstantin.hemker@googlemail.com>
2024-01-15 15:06:01 +0000

    TESTS: minor docstring

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,9 @@
 import pytest
 from mmhb.utils import *
 
+
 def test_config():
+    # config smoke test
     config = Config("config/config.yml").read()
 
-    assert hasattr(config, 'data_path')
+    assert hasattr(config, "data_path")
