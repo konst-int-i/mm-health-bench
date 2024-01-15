e8976090af4b4d5f75465d6c0d98db8dde9dba7c
konst-int-i <konstantin.hemker@googlemail.com>
2024-01-15 09:28:04 +0000

    ETL: add base loaders and tests

--- mmhb/loader/base.py
+++ mmhb/loader/base.py
@@ -8,7 +8,9 @@ class MMDataset(Dataset):
     """
     Generic torch dataset object for supervised multi-modal data.
     """
-    def __init__(self, config: Union[str, Path]):
+    def __init__(self,
+                 config: Union[str, Path],
+                 ):
         """
         Args:
             tensors(List[torch.Tensor]): modalities for each sample, note that the first dim of each tensor is
@@ -20,23 +22,26 @@ class MMDataset(Dataset):
         self.config = Config(config).read()
         self.path = self.config.data_path
         self.tensors = None
+        self.target = None
 
     def __getitem__(self, idx) -> [Tuple[List[torch.Tensor], torch.Tensor], List[torch.Tensor]]:
         # to be implemented in each child class
-        pass
+        raise NotImplementedError
 
     def __len__(self):
-        if self.tensors is None:
-            return 0
-        else:
-            return self.tensors[0].size()[0]
+        return 0 if self.tensors is None else self.tensors[0].size()[0]
 
     @property
     def num_modalities(self):
-        return len(self.tensors)
+        return 0 if self.tensors is None else len(self.tensors)
 
 class MMSampleDataset(MMDataset):
-    pass
+    def __init__(self, config: Union[str, Path], tensors: List[torch.Tensor], target: torch.Tensor):
+        super().__init__(config)
+        self.tensors = tensors
+        self.target = target
+
+
 
 if __name__ == "__main__":
 
---
+++ tests/test_loaders.py
@@ -0,0 +1,31 @@
+import torch
+import pytest
+from mmhb.loader import *
+
+def test_base():
+    data = MMDataset(config="config/config.yml")
+
+    with pytest.raises(NotImplementedError):
+        data[0]
+    assert (len(data)) == 0
+    assert data.num_modalities == 0
+
+def test_sample_dataset():
+
+    n = 500
+    tab = torch.randn(n, 1, 50)
+    img = torch.randn(n, 512, 512, 3)
+    patch_img = torch.randn(n, 100, 1024)
+    seq = torch.randn(n, 1, 100)
+    target = torch.randn(n)
+
+    # Generate list of tensors
+    tensors = [tab, patch_img, seq]
+
+    data = MMSampleDataset(config="config/config.yml",
+                           tensors=tensors,
+                           target=target
+                           )
+
+    assert len(data) == n
+    assert data.num_modalities == 3
