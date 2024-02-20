535d8cf0ffd2720915a498d78124b89e7a5babb7
konst-int-i <konstantin.hemker@googlemail.com>
2024-02-20 11:47:57 +0000

    LOGGIN: fix log level

--- mmhb/utils/logging.py
+++ mmhb/utils/logging.py
@@ -2,15 +2,17 @@ import logging
 import colorlog
 from mmhb.utils.config import Config
 
+
 def setup_logging():
     log_conf = Config("config/logging.yml").read()
-    log_level = logging.DEBUG if log_conf.level == "debug" else logging.INFO
+    # log_level = logging.DEBUG if log_conf.level == "debug" else logging.INFO
 
     handler = logging.StreamHandler()
     handler.setFormatter(_colour_formatter(log_conf))
-    logging.basicConfig(level=log_level, handlers=[handler])
+    logging.basicConfig(level=logging.INFO, handlers=[handler])
     logger = logging.getLogger(__name__)
     return logger
+
 
 def _colour_formatter(config: Config):
     formatter = colorlog.ColoredFormatter(
@@ -18,15 +20,18 @@ def _colour_formatter(config: Config):
         datefmt="%Y-%m-%d %H:%M:%S",
         reset=True,
         log_colors={
-            'DEBUG':    'white',
-            'INFO':     'cyan',
-            'WARNING':  'yellow',
-            'ERROR':    'red',
-            'CRITICAL': 'red,bg_white',
-        }
+            "DEBUG": "white",
+            "INFO": "cyan",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "red,bg_white",
+        },
     )
     return formatter
 
+
 def _check_config(config: Config):
     valid_levels = ["debug", "info"]
-    assert config.level in valid_levels, f"Invalid logging level, must be one of {valid_levels}"
+    assert (
+        config.level in valid_levels
+    ), f"Invalid logging level, must be one of {valid_levels}"
